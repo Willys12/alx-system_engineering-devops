079f116b9cc1edf2b0ef877b80a3c0abb03570a1
Willys12 <ben.willys92@gmail.com>
2024-04-24 12:46:13 +0300

    Correction on files

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,12 +1,4 @@
 # Kills a process named killmenow.
-exec { 'killmenow':
-  command     => 'pkill killmenow',
-  onlyif      => 'pgrep killmenow',
-  path        => '/bin:/usr/bin',
-  refreshonly => true,
-}
-# Notifies that the process is terminated.
-notify { 'successfull':
-  message => 'Terminated',
-  require => Exec['killmenow'],
+exec { 'pkill killmenow':
+  path => 'usr/bin:usr/sbin:/bin',
 }
