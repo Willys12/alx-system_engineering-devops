5c7ce5f615bdacc08198288d134cdd8353fd715b
Willys12 <ben.willys92@gmail.com>
2024-06-05 13:44:19 -0700

    Correction

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         user_id = user.get('id')
         username = user.get('username')
         todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'
-        .format(user_id)
+.format(user_id)
         todos_resp = requests.get(todos_url)
         tasks = todos_resp.json()
 
