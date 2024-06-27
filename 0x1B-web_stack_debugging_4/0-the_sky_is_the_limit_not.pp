ec8aa9270111680fdcc230e88fd33bb08b76d0b1
Willys12 <ben.willys92@gmail.com>
2024-06-27 13:37:52 +0000

    Fixing nginx server

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increasing the ULIMIT of default file
+exec { 'fix--for-nginx':
+  # ULIMIT value modification
+  command  => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  # sed command path
+  path	   => '/usr/local/bin/:/bin/',
+}
+
+# Restart nginx
+exec { 'nginx_restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
